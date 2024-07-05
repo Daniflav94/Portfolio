34c7c2215eba232d728f1e9d6df54a6d5e3b469b
Daniflav94 <danyelly_mell@hotmail.com>
2024-07-05 14:28:15 -0300

    remoção link deploy projeto

--- src/utils/mocks.js
+++ src/utils/mocks.js
@@ -30,9 +30,8 @@ export const projects = [
     title: "Gestão de ativos",
     stacks: ["react", "node", "typescript", "express"],
     github: "https://github.com/Daniflav94/Gestao-Ativos",
-    deploy: "https://gestao-ativos-smoky.vercel.app/",
     description:
-      "Aplicação web desenvolvida para gerenciar ativos, no qual é possível cadastrar os ativos e colaboradores e fazer apontamentos como: trocar status do ativo para alocado e relacionar com um colaborador e também mudar status para manutenção ou desabilitado. Todos os apontamentos são salvos e os mais recentes são exibidos em uma tabela, na qual o usuário poderá realizar diversos filtros e gerar um relatório em pdf dos resultados. Para testar a aplicação utilize - login: prod1@email.com, senha: 123456",
+      "Aplicação web desenvolvida para gerenciar ativos, no qual é possível cadastrar os ativos e colaboradores e fazer apontamentos como: trocar status do ativo para alocado e relacionar com um colaborador e também mudar status para manutenção ou desabilitado. Todos os apontamentos são salvos e os mais recentes são exibidos em uma tabela, na qual o usuário poderá realizar diversos filtros e gerar um relatório em pdf dos resultados.",
     video: gestaoAtivosGif,
     date: "06/2024",
   },
